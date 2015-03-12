2affc6925d52c7ad58dd29c06b8131b4f28f62c0
hezihezi <hj2393@columbia.edu>
2015-03-12 18:52:25 -0400

    changed the styles of the bars by adding an column

--- scripts.js
+++ scripts.js
@@ -19,6 +19,11 @@
       		smallArray.push(everyTen.years);
       		smallArray.push(everyTen.persons);
       		
+      		if (everyTen.years == "2010s"){
+      		smallArray.push('color: #6AA121;fill-opacity: 0.2')
+      	}else
+      		smallArray.push('color: #6AA121');		
+      		
       		bigArray.push(smallArray);
       	
       }//end of for loop
@@ -27,22 +32,21 @@
       var data = new google.visualization.DataTable();
       data.addColumn('string', 'years');
       data.addColumn('number', 'persons');
+      data.addColumn({type:'string', role:'style'})
       data.addRows(bigArray);
       
       var numberFormatter = new google.visualization.NumberFormat({fractionDigits:0});
       numberFormatter.format(data,1);
 
       var options = {
-        width: 1000,
-        height: 563,
-        hAxis: {
-          title: 'Time of Day',
-        },
-        vAxis: {
-          title: 'Rating (scale of 1-10)'
-        }
+        hAxis: {},
+        vAxis: {}
       };
 
+		options.hAxis
+		options.vAxis.title = "persons obtaining green cards in the given decade"
+		
+		
       var chart = new google.visualization.ColumnChart(
         document.getElementById('chart_div'));
 
