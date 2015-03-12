36e62c77c0df3c2ee16a5df0497c20cf9d565aa7
hezihezi <hj2393@columbia.edu>
2015-03-12 00:57:07 -0400

    wrote a for loop to push the years and persons into small var smallArray to var bigArray so that the data is in an array of arrays format

--- scripts.js
+++ scripts.js
@@ -7,31 +7,33 @@
     // instantiates the column chart, passes in the data and
     // draws it.
     function drawChart() {
+    	
+      var bigArray = [];
+      
+      for (var i=0; i<ChineseGreenCardData.length; i++){
+      		
+      		var smallArray = [];
+      		
+      		var everyTen = ChineseGreenCardData[i];
+      		
+      		smallArray.push(everyTen.years);
+      		smallArray.push(everyTen.persons);
+      		
+      		bigArray.push(smallArray);
+      	
+      }//end of for loop
 
+      
       var data = new google.visualization.DataTable();
-      data.addColumn('timeofday', 'Time of Day');
-      data.addColumn('number', 'Motivation Level');
-
-      data.addRows([
-        [{v: [8, 0, 0], f: '8 am'}, 1],
-        [{v: [9, 0, 0], f: '9 am'}, 2],
-        [{v: [10, 0, 0], f:'10 am'}, 3],
-        [{v: [11, 0, 0], f: '11 am'}, 4],
-        [{v: [12, 0, 0], f: '12 pm'}, 5],
-        [{v: [13, 0, 0], f: '1 pm'}, 6],
-        [{v: [14, 0, 0], f: '2 pm'}, 7],
-        [{v: [15, 0, 0], f: '3 pm'}, 8],
-        [{v: [16, 0, 0], f: '4 pm'}, 9],
-        [{v: [17, 0, 0], f: '5 pm'}, 10],
-      ]);
+      data.addColumn('string', 'years');
+      data.addColumn('number', 'persons');
+      data.addRows(bigArray);
 
       var options = {
         width: 1000,
         height: 563,
         hAxis: {
           title: 'Time of Day',
-          format: 'h:mm a',
-          gridlines: {count: 10}
         },
         vAxis: {
           title: 'Rating (scale of 1-10)'
